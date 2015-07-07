58201c366b7f286b688a486b614f222dcfdfd405
root <root@dev32.(none)>
2015-07-07 17:13:16 +0200

    Committer: Pepe <root@dev32.(none)>
    On branch master
    
    [BugFix] Unable to restore a backup with the WEBGUI
    
    	modified:   usr/local/zenloadbalancer/app/zenbackup/zenbackup.pl

--- usr/local/zenloadbalancer/app/zenbackup/zenbackup.pl
+++ usr/local/zenloadbalancer/app/zenbackup/zenbackup.pl
@@ -31,11 +31,11 @@ $action = $ARGV[1];
 
 if ($action eq "-c")
 	{
-	my @eject = `$tar zcvf $backupdir\/backup-$name.tar.gz $backupfor`;
+	my @eject = `$tar -czvf $backupdir\/backup-$name.tar.gz $backupfor`;
 	}
 
 if ($action eq "-d")
 	{
-	my @eject = `$tar -xvzf $backupdir\/backup-$name.tar.gz -C /`;
+	my @eject = `$tar -xzvf $backupdir\/backup-$name.tar.gz -C /`;
 	}
 
