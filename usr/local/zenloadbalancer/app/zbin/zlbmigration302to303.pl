90cd5d1a85250536e972abf13d2476e3bfa3e01d
Jose Pablo <josepgl@sofintel.net>
2016-01-25 17:48:03 +0100

    [Improvement] Merge support branch to master
    Signed-off-by: Jose Pablo <josepgl@sofintel.net>
    
    	new file:   usr/local/zenloadbalancer/app/zbin/supportsave
    	new file:   usr/local/zenloadbalancer/app/zbin/zlbmigration302to303.pl
    	new file:   usr/local/zenloadbalancer/app/zbin/zlbmigrationto402.sh

---
+++ usr/local/zenloadbalancer/app/zbin/zlbmigration302to303.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+###############################################################################
+#
+#     This file is part of the Zen Load Balancer Enterprise Edition software 
+#     package.
+#
+#     Copyright (C) 2014 SOFINTEL IT ENGINEERING SL, Sevilla (Spain)
+#
+#     This file cannot be distributed, released in public domain and/or for 
+#     commercial purposes.
+#
+###############################################################################
+
+#this script migrates configuration files from v3.02 to v3.03
+#only supported for Zen Load Balancer Enterprise Edition.
+
+print "This script modifies the configuration files from ZEN Load Balancer Enterprise Edition beyond v3.02...\n";
+### migrate http and https configuration files
+@httpfarms = `ls /usr/local/zenloadbalancer/config/*_pound.cfg 2> /dev/null`;
+foreach $file(@httpfarms){
+
+    chomp($file);
+    use Tie::File;
+    tie @filelines, 'Tie::File', "$file";
+    if ( ! grep( /^ConnTO/,@filelines) ){
+        print "farm file $file (connection params) needs to be migrated...\n";
+        splice @filelines,14,0,"ConnTO\t\t20";
+        print "param migrated\n";
+
+    }
+
+    if ( ! grep( /\tRewriteLocation/,@filelines) ){
+	print "farm file $file (rewrite params) needs to be migrated...\n";
+	splice @filelines,31,0,"\tRewriteLocation 0";
+	print "param migrated\n";
+    }
+
+
+    untie $filelines;
+
+}
+
+
+
+### migrate l4 configuration files
+my @l4farms = `ls /usr/local/zenloadbalancer/config/*_l4txnat.cfg 2> /dev/null`;
+foreach $file(@l4farms){
+	chomp($file);
+	use Tie::File;
+	tie @filelines, 'Tie::File', "$file";
+	print "farm file $file needs to be migrated...\n";
+	my $line = @filelines[0];
+	my @args = split("\;",$line);
+	my $line = "@args[0]\;tcp\;@args[1]\;@args[2]\;@args[3]\;@args[4]\;@args[5]\;@args[6]\;@args[7]";
+	@filelines[0] = $line;
+	print "file migrated\n";
+	untie $file;
+	$oldfile = $file;
+	$file =~ s/_l4txnat.cfg/_l4xnat.cfg/;
+	rename $oldfile,$file;
+}
+my @l4farms = `ls /usr/local/zenloadbalancer/config/*_l4uxnat.cfg 2> /dev/null`;
+foreach $file(@l4farms){
+	chomp($file);
+	use Tie::File;
+	tie @filelines, 'Tie::File', "$file";
+	print "farm file $file needs to be migrated...\n";
+	my $line = @filelines[0];
+	my @args = split("\;",$line);
+	my $line = "@args[0]\;udp\;@args[1]\;@args[2]\;@args[3]\;@args[4]\;@args[5]\;@args[6]\;@args[7]";
+	@filelines[0] = $line;
+	print "file migrated\n";
+	untie $file;
+	$oldfile = $file;
+	$file =~ s/_l4uxnat.cfg/_l4xnat.cfg/;
+	rename $oldfile,$file;
+}
+
+
+### migrate cluster
+$clfile = "/usr/local/zenloadbalancer/config/cluster.conf";
+if ( -e $clfile){
+
+    use Tie::File;
+    tie @filelines, 'Tie::File', "$clfile";
+    if ( ! grep( /^DEADRATIO/,@filelines) ){
+        print "Cluster file $clfile needs to be migrated...\n";
+        push(@filelines,"DEADRATIO:2");
+	print "Cluster file migrated\n";
+    }
+
+    untie $clfile;
+}
+
---
+++ usr/local/zenloadbalancer/app/zbin/zlbmigrationto402.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+tplfile="/usr/local/zenloadbalancer/app/pound/etc/poundtpl.cfg"
+confdir="/usr/local/zenloadbalancer/config"
+dcookie="BackendCookie"
+dssl="DisableSSLv3"
+lcookie="#BackendCookie \"ZENSESSIONID\" \"domainname.com\" \"/\" 0"
+ddhparam="DHParams"
+ldhparam="DHParams 	\"/usr/local/zenloadbalancer/app/pound/etc/dh2048.pem\""
+decdh="ECDHCurve"
+lecdh="ECDHCurve 	\"prime256v1\""
+dhfile="/usr/local/zenloadbalancer/app/pound/etc/dh2048.pem"
+openssl=`which openssl`
+
+cd $confdir
+if [ ls *_pound.cfg 2>/dev/null ]
+then
+	if [ "`grep 'Disable SSLv3' $tplfile`" != "" ]; then
+		for file in $(grep -l $dssl *_pound.cfg); do
+			sed -i "s/$dssl/Disable\ SSLv3/g" $file
+			echo "Directive $dssl migrated in the file $file"
+		done
+	fi
+	if [ "`grep $dcookie $tplfile`" != "" ]; then
+		for file in $(ls -1 *_pound.cfg); do
+			if [ "`grep $dcookie $file`" == "" ]; then
+				sed -e "/DynScale/ a$lcookie"  $file | sed -e "s?$lcookie?\t\t$lcookie?g" > /tmp/migratefile.txt
+				mv /tmp/migratefile.txt $file
+				echo "Directive $dcookie added to file $file"
+			fi
+		done
+	fi
+	if [ "`grep $ddhparam $tplfile`" != "" ]; then
+		if [ ! -f $dhfile ]; then
+			$openssl dhparam -5 2048 -out $dhfile
+		fi
+		for file in $(ls -1 *_pound.cfg); do
+			if [ "`grep $ddhparam $file`" == "" ]; then
+				sed -i "/^Control/ a$ldhparam" $file
+				sync
+				sed -i "/^$ddhparam/ a$lecdh" $file
+				echo "Directives $ddhparam and $decdh added to file $file"
+			fi
+		done
+	fi
+fi
+cd - > /dev/null
+
+
+exit 0
