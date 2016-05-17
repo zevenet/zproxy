f305a2201e0943b338fa907f648fe69cc1ddda05
Jose Pablo <josepgl@sofintel.net>
2016-05-17 17:12:22 +0200

    [Bugfix] Fix regression bug in installation messages with no HTTP farms.
    Signed-off-by: Jose Pablo <josepgl@sofintel.net>
    
    	modified:   usr/local/zenloadbalancer/app/zbin/zlbmigrationto402.sh

--- usr/local/zenloadbalancer/app/zbin/zlbmigrationto402.sh
+++ usr/local/zenloadbalancer/app/zbin/zlbmigrationto402.sh
@@ -11,34 +11,35 @@ lecdh="ECDHCurve 	\"prime256v1\""
 dhfile="/usr/local/zenloadbalancer/app/pound/etc/dh2048.pem"
 openssl=`which openssl`
 
-cd $confdir
-
-for file in $(grep -l $dssl *_pound.cfg); do
-	sed -i "s/$dssl/Disable\ SSLv3/g" $file
-	echo "Directive $dssl migrated in the file $file"
-done
-
-for file in $(ls -1 *_pound.cfg); do
-	if [ "`grep $dcookie $file`" == "" ]; then
-		sed -e "/DynScale/ a$lcookie"  $file | sed -e "s?$lcookie?\t\t$lcookie?g" > /tmp/migratefile.txt
-		mv /tmp/migratefile.txt $file
-		echo "Directive $dcookie added to file $file"
-	fi
-done
-
 if [ ! -f $dhfile ]; then
 	$openssl dhparam -5 2048 -out $dhfile
 fi
 
-for file in $(ls -1 *_pound.cfg); do
-	if [ "`grep $ddhparam $file`" == "" ]; then
-		sed -i "/^Control/ a$ldhparam" $file
-		sync
-		sed -i "/^$ddhparam/ a$lecdh" $file
-		echo "Directives $ddhparam and $decdh added to file $file"
-	fi
-done
-cd - > /dev/null
+cd $confdir
+if [ ls *_pound.cfg 2>/dev/null ]
+then
+	for file in $(grep -l $dssl *_pound.cfg); do
+		sed -i "s/$dssl/Disable\ SSLv3/g" $file
+		echo "Directive $dssl migrated in the file $file"
+	done
 
+	for file in $(ls -1 *_pound.cfg); do
+		if [ "`grep $dcookie $file`" == "" ]; then
+			sed -e "/DynScale/ a$lcookie"  $file | sed -e "s?$lcookie?\t\t$lcookie?g" > /tmp/migratefile.txt
+			mv /tmp/migratefile.txt $file
+			echo "Directive $dcookie added to file $file"
+		fi
+	done
+
+	for file in $(ls -1 *_pound.cfg); do
+		if [ "`grep $ddhparam $file`" == "" ]; then
+			sed -i "/^Control/ a$ldhparam" $file
+			sync
+			sed -i "/^$ddhparam/ a$lecdh" $file
+			echo "Directives $ddhparam and $decdh added to file $file"
+		fi
+	done
+fi
+cd - > /dev/null
 
 exit 0
