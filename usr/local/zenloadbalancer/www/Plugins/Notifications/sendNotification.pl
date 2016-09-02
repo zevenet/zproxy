f817af24557215e3ac91745208122b0a7ff00e05
Alvaro <alvarocb@sofintel.net>
2016-09-02 13:38:23 +0200

    [Improvement] Notification plugin sends mail when backend changes status
    Signed-off-by: Alvaro <alvarocb@sofintel.net>
    
    	modified:   Notifications/sec.rules
    	modified:   Notifications/sendNotification.pl
    	deleted:    Notifications/syslog.pl

--- usr/local/zenloadbalancer/www/Plugins/Notifications/sendNotification.pl
+++ usr/local/zenloadbalancer/www/Plugins/Notifications/sendNotification.pl
@@ -25,167 +25,206 @@
 
 use Config::Tiny;
 
-(my $section, my $pattern) = @ARGV;
+( my $section, my $pattern ) = @ARGV;
 
 my $body;
 my $command;
-my ($subject, $bodycomp) = &getSubjectBody($pattern);
+my ( $subject, $bodycomp ) = &getSubjectBody( $pattern );
 
-if ($subject eq "error")
-{		
+if ( $subject eq "error" || !$bodycomp )
+{
 	exit 1;
 }
 
-
-$body = "
-***** Notifications *****
-
-Alerts: $section Notification
-";
+$body = "\n***** Notifications *****\n\n" . "Alerts: $section Notification\n";
 $body .= $bodycomp;
 
-$command .= &getData('bin');
-$command .= " --to " . &getData('to');
-$command .= " --from " . &getData('from');
-$command .= " --server " . &getData('server');
-$command .= " --auth " . &getData('auth');
-$command .= " --auth-user " . &getData('auth-user');
-$command .= " --auth-password " . &getData('auth-password');
+$command .= &getData( 'bin' );
+$command .= " --to " . &getData( 'to' );
+$command .= " --from " . &getData( 'from' );
+$command .= " --server " . &getData( 'server' );
+$command .= " --auth " . &getData( 'auth' );
+$command .= " --auth-user " . &getData( 'auth-user' );
+$command .= " --auth-password " . &getData( 'auth-password' );
 if ( 'true' eq &getData( 'tls' ) ) { $command .= " -tls"; }
-$command .= " --header \"Subject: " . &getData('PrefixSubject', $section) . " $subject\"";
+$command .=
+    " --header \"Subject: "
+  . &getData( 'PrefixSubject', $section )
+  . " $subject\"";
 $command .= " --body \"$body\"";
+
 #not print
 $command .= " 1>/dev/null";
-
 
 #~ print "$body";
 system ( $command );
 
-
-
 # return:   @array = [ $subject, $body ]
 # my ( $subject, $body ) = getSubjectBody( $msg )
-sub getSubjectBody   # &getSubjectBody ( $msg ) 
+sub getSubjectBody    # &getSubjectBody ( $msg )
 {
 	my ( $msg ) = @_;
-	
+
 	my @output;
 	my $subject;
 	my $body;
-	
-	my ($date, $host, $program, $ip, $port, $status, $farm, $service);
+
+	my ( $date, $host, $program, $ip, $port, $status, $farm, $service, $pid );
 	my $auxBody;
 
 	# Keep date, host and program from log msg
-	if ( $msg =~ s/^(\w+\s+\d+\s+\d+:\d+:\d+)\s+(\w+)\s+(\w+)(?:\[\d+\])?\: // )
+	# Sep  1 15:59:06 maqvir gdnsd[18513]:
+	if ( $msg =~ s/(\w+\s+\d+\s+\d+:\d+:\d+)\s+(\w+)\s+(\w+)(\[\d+\])?\: // )
 	{
-		$date = $1;
-		$host = $2;
+		$date    = $1;
+		$host    = $2;
 		$program = $3;
+		$pid     = $4;
+		$pid =~ s/[\[\]]//g;
 	}
 
-	# pound msg
+	# Gdnsd msg
 	# example:
-	# Jul 28 11:16:46 maqvir pound: (7f4dccf24700) BackEnd 192.168.0.172:80 dead (killed) in farm: 'test', service: 'srv1'
-	if ( ( $program =~ /pound/ || $program =~ /farmguardian/ ) &&
-		 $msg =~ /BackEnd (\d+\.\d+\.\d+\.\d+):(\d+) (\w+)(?: \(\w+\))? in farm: '(\w+)', service: '(\w+)'/ )
+	# state of '192.168.0.167/tcp_80' changed from UP/5 to DOWN/10
+	if (    $program =~ /gdnsd/
+		 && $msg =~
+		 /state of \'(\d+\.\d+\.\d+\.\d+)\/(tcp|.+_fg)_(\d+)\' .+ to (DOWN|UP)/ )
 	{
-		$ip = $1;
-		$port = $2;
-		$status = $3;
-		$farm = $4;
-		$service = $5;
-		
-		if ( $program =~ /pound/ ) { $program = "It has been"; }
-		elsif ($program =~ /farmguardian/ ) { $program = "Farmguardian"; }
-		
-		if ( $status =~ /dead/ || $status =~ /down/ )
-			{ $status = "DOWN"; }
-		elsif ( $status =~ /resurrect/ )
-			{ $status = "UP"; }
+		$ip      = $1;
+		$port    = $3;
+		$status  = $4;
+		$service = $2;
+		$farm    = &getGSLBFarm( $pid );
 
-		if ($farm && !$service)
-			{ $auxBody = "(farm: '$farm')"; }
-		if ($farm && $service)
-			{ $auxBody = "(farm: '$farm', service: '$service')"; }
+		if ( $service eq tcp )
+		{
+			$program = "Farmguardian";
+		}
 
-		$body .= "
-$program detected a status change to $status $auxBody
+		$service =~ s/^tcp$//;
+		$service =~ s/_fg//;
 
-Zen Server: $host
-Backend: $ip
-Port: $port
-		";
-		
-		$subject = "Backend $ip changed to $status"
-		
+		if ( $farm && !$service ) { $auxBody = "(farm: '$farm')"; }
+		if ( $farm && $service )  { $auxBody = "(farm: '$farm', service: '$service')"; }
+
+		$body .= "\n"
+		  . "$program detected a status change to $status $auxBody\n"
+		  . "Zen Server: $host\n"
+		  . "Backend: $ip\n"
+		  . "Port: $port\n";
+
+		$subject = "Backend $ip changed to $status";
+
 	}
+
+# pound msg
+# example:
+# (7f4dccf24700) BackEnd 192.168.0.172:80 dead (killed) in farm: 'test', service: 'srv1'
+	elsif ( ( $program =~ /pound/ || $program =~ /farmguardian/ )
+		&& $msg =~
+		/BackEnd (\d+\.\d+\.\d+\.\d+):(\d+) (\w+)(?: \(\w+\))? in farm: '(\w+)', service: '(\w+)'/
+	  )
+	{
+		$ip      = $1;
+		$port    = $2;
+		$status  = $3;
+		$farm    = $4;
+		$service = $5;
+
+		if    ( $program =~ /pound/ )        { $program = "It has been"; }
+		elsif ( $program =~ /farmguardian/ ) { $program = "Farmguardian"; }
+
+		if ( $status =~ /dead/ || $status =~ /down/ ) { $status = "DOWN"; }
+		elsif ( $status =~ /resurrect/ ) { $status = "UP"; }
+
+		if ( $farm && !$service ) { $auxBody = "(farm: '$farm')"; }
+		if ( $farm && $service )  { $auxBody = "(farm: '$farm', service: '$service')"; }
+
+		$body .= "\n"
+		  . "$program detected a status change to $status $auxBody\n"
+		  . "Zen Server: $host\n"
+		  . "Backend: $ip\n"
+		  . "Port: $port\n";
+
+		$subject = "Backend $ip changed to $status";
+
+	}
+
 	# Cluster msg
 	# example:
-	# Aug 16 11:47:58 maqvir ucarp[11541]: [WARNING] Switching to state: MASTER
-	elsif ( $program =~ /ucarp/ &&
-			$msg =~ /\[WARNING\] Switching to state: (\w+)/ )
+	# [WARNING] Switching to state: MASTER
+	elsif (    $program =~ /ucarp/
+			&& $msg =~ /\[WARNING\] Switching to state: (\w+)/ )
 	{
 		$status = $1;
-		
-		$body .= "
-Zen server switched to $status
 
-Zen Server: $host
-Current Status: $status
-		";
-		
-		$subject = "Zen server \"$host\" switched to $status"
-		
+		$body .= "\n"
+		  . "Zen server switched to $status\n"
+		  . "Zen Server: $host\n"
+		  . "Current Status: $status\n";
+
+		$subject = "Zen server \"$host\" switched to $status";
+
 	}
-	
+
 	else
 	{
 		$subject = "error";
 	}
-	
-	
-	$body .="	
 
-Date/Time: $date
-
-";		
+	$body .= "\n\nDate/Time: $date\n\n";
 
 	push @output, $subject, $body;
-	return @output;	
+	return @output;
 }
-
 
 #  &getData ( $key, $section )
 #  &getData ( $key )
-sub getData   
+sub getData
 {
 	my ( $key, $section ) = @_;
 	my $argumentos = scalar @_;
 	my $data;
 	my $fileHandle;
 	my $fileName;
-						
-			
+
 	if ( $argumentos == 1 )
-	{	
-		$section = 'Smtp';
+	{
+		$section  = 'Smtp';
 		$fileName = "/usr/local/zenloadbalancer/www/Plugins/Notifications/Senders.conf";
 	}
 	else
 	{
 		$fileName = "/usr/local/zenloadbalancer/www/Plugins/Notifications/Alerts.conf";
 	}
-			
+
 	if ( !-f $fileName )
-	{ 
-		print "don't find $fileName."; 
+	{
+		print "don't find $fileName.";
 	}
 	else
 	{
 		$fileHandle = Config::Tiny->read( $fileName );
-		$data = $fileHandle->{ $section }->{ $key };
+		$data       = $fileHandle->{ $section }->{ $key };
 	}
 
 	return $data;
+}
+
+#   &getGSLBFarm ( $pid )
+sub getGSLBFarm
+{
+	my ( $pid ) = @_;
+	my $farm;
+
+	my @aux = `ps -ef | grep $pid `;
+
+	$pr = @aux;
+
+	foreach my $line ( @aux )
+	{
+		if ( $line =~ /config\/(.+)_gslb\.cfg/ ) { $farm = $1; }
+	}
+
+	return $farm;
 }
