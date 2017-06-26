2522d98168fdbf576ab33da2bebaa7d68ffa46ab
Jose Pablo <josepablo.garcia@zevenet.com>
2017-06-26 18:43:42 +0200

    [Improvement] Move activation certificate functions out of zapi.cgi
    Signed-off-by: Jose Pablo <josepablo.garcia@zevenet.com>
    
    	new file:   usr/share/perl5/Zevenet/Certificate/Activation.pm

---
+++ usr/share/perl5/Zevenet/Certificate/Activation.pm
@@ -0,0 +1,217 @@
+#!/usr/bin/perl
+###############################################################################
+#
+#    Zevenet Software License
+#    This file is part of the Zevenet Load Balancer software package.
+#
+#    Copyright (C) 2014-today ZEVENET SL, Sevilla (Spain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+use strict;
+
+use Time::Local;
+#~ use Date::Parse qw(str2time);
+#~ use Time::localtime qw(ctime);
+use Zevenet::SystemInfo;
+
+# build local key
+sub keycert    # ()
+{
+	# requires:
+	#~ use Sys::Hostname;
+
+	my $dmidecode_bin = "/usr/sbin/dmidecode";    # input
+	my $hostname      = &getHostname();           # input
+
+	my @dmidec  = `$dmidecode_bin`;
+	my @dmidec2 = grep ( /UUID\:/, @dmidec );
+	my $dmi     = $dmidec2[0];
+
+	$dmi =~ s/\"//g;     # remove doble quotes
+	$dmi =~ s/^\s+//;    # remove whitespaces at the begining
+	$dmi =~ s/\s+$//;    # remove whitespaces at the end
+	$dmi =~ s/\ //g;     # remove spaces
+
+	my @dmidec3 = split ( ":", $dmi );
+	$dmi = $dmidec3[1];
+
+	$hostname =~ s/\"//g;     # remove doble quotes
+	$hostname =~ s/^\s+//;    # remove whitespaces at the begining
+	$hostname =~ s/\s+$//;    # remove whitespaces at the end
+
+	my $encrypted_string  = crypt ( "${dmi}${hostname}", "93" );
+	my $encrypted_string2 = crypt ( "${hostname}${dmi}", "a3" );
+	my $encrypted_string3 = crypt ( "${dmi}${hostname}", "ZH" );
+	my $encrypted_string4 = crypt ( "${hostname}${dmi}", "h7" );
+	$encrypted_string =~ s/^93//;
+	$encrypted_string2 =~ s/^a3//;
+	$encrypted_string3 =~ s/^ZH//;
+	$encrypted_string4 =~ s/^h7//;
+
+	my $str =
+	  "${encrypted_string}-${encrypted_string2}-${encrypted_string3}-${encrypted_string4}";
+
+	$str =~ s/\"//g;     # remove doble quotes
+	$str =~ s/^\s+//;    # remove whitespaces at the begining
+	$str =~ s/\s+$//;    # remove whitespaces at the end
+
+	return $str;
+}
+
+# evaluate certificate
+sub certcontrol          # ()
+{
+	# requires:
+	#~ use Date::Parse;
+	#~ use Time::localtime;
+	require Zevenet::SystemInfo;
+
+	my $basedir = &getGlobalConfiguration( 'basedir' );
+
+	# input
+	my $hostname    = &getHostname();
+	my $zlbcertfile = "$basedir/zlbcertfile.pem";
+	my $openssl_bin = "/usr/bin/openssl";
+	my $keyid       = "4B:1B:18:EE:21:4A:B6:F9:76:DE:C3:D8:86:6D:DE:98:DE:44:93:B9";
+	my $key         = &keycert();
+
+	# output
+	my $swcert = 0;
+
+	if ( -e $zlbcertfile )
+	{
+		my @zen_cert = `$openssl_bin x509 -in $zlbcertfile -noout -text 2>/dev/null`;
+
+		if (    ( !grep /$key/, @zen_cert )
+			 || ( !grep /keyid:$keyid/,   @zen_cert )
+			 || ( !grep /CN=$hostname\/|CN = $hostname\,/, @zen_cert ) )
+		{
+			$swcert = 2;
+		}
+
+		#~ my $now = ctime(); ############# From: Time::localtime #############
+		my $now = scalar localtime; ############# From: Time::localtime #############
+		&zenlog( "now: $now" );
+
+		# Certificate validity date
+		my @notbefore = grep /Not Before/i, @zen_cert;
+		my $nb = join '', @notbefore;
+		$nb =~ s/not before.*:\ //i;
+		$nb =~ s/^\s*//;
+
+		#~ my $ini = str2time( $nb ); ############# From: Date::Parse #############
+		&zenlog( "Activation certificate expedition date: $nb" );
+		my ( $month, $day, $hours, $min, $sec, $year ) = split /(?: |:)/, $nb;
+		my $ini = timelocal( $sec, $min, $hours, $day, $month, $year );
+		&zenlog( "ini: $ini" );
+
+		# Certificate expiring date
+		my @notafter = grep /Not After/i, @zen_cert;
+		my $na = join "", @notafter;
+		$na =~ s/not after.*:\ //i;
+		$na =~ s/^\s*//;
+
+		#~ my $end = str2time( $na ); ############# From: Date::Parse #############
+		&zenlog( "Activation certificate expiration date: $na" );
+		( $month, $day, $hours, $min, $sec, $year ) = split /(?: |:)/, $na;
+		my $end = timelocal( $sec, $min, $hours, $day, $month, $year );
+		&zenlog( "end: $end" );
+
+		# Validity remaining
+		my $totaldays = ( $end - $ini ) / 86400;
+		$totaldays =~ s/\-//g;
+		my $dayright = ( $end - time () ) / 86400;
+
+		#control errors
+		if ( $totaldays < 364 && $dayright < 0 && $swcert == 0 )
+		{
+			# Policy: expired testing certificates would not stop zen service,
+			# but rebooting the service would not start the service,
+			# interfaces should always be available.
+			$swcert = 3;
+		}
+
+		if ( $totaldays > 364 && $dayright < 0 && $swcert == 0 )
+		{
+			# The contract support plan is expired you have to request a
+			# new contract support. Only message alert!
+			$swcert = -1;
+		}
+	}
+	else
+	{
+		#There isn't certificate in the machine
+		$swcert = 1;
+	}
+
+	# error codes
+	#swcert = 0 ==> OK
+	#swcert = 1 ==> There isn't certificate
+	#swcert = 2 ==> Cert isn't signed OK
+	#swcert = 3 ==> Cert test and it's expired
+	#swcert = -1 ==> Cert support and it's expired
+
+	#output
+	return $swcert;
+}
+
+sub checkActivationCertificate
+{
+	my $swcert = &certcontrol();
+
+	# if $swcert is greater than 0 zapi should not work
+	if ( $swcert > 0 )
+	{
+		my $message;
+
+		if ( $swcert == 1 )
+		{
+			$message =
+			  "There isn't a valid Zen Load Balancer certificate file, please request a new one";
+		}
+		elsif ( $swcert == 2 )
+		{
+			$message =
+			  "The certificate file isn't signed by the Zen Load Balancer Certificate Authority, please request a new one";
+		}
+		elsif ( $swcert == 3 )
+		{
+			# Policy: expired testing certificates would not stop zen service,
+			# but rebooting the service would not start the service,
+			# interfaces should always be available.
+			$message =
+			  "The Zen Load Balancer certificate file you are using is for testing purposes and its expired, please request a new one";
+		}
+
+		&httpResponse(
+					   {
+						 code => 403,
+						 body => {
+								   message         => $message,
+								   certificate_key => &keycert(),
+								   hostname        => &getHostname(),
+						 }
+					   }
+		);
+
+		exit;
+	}
+
+	return $swcert;
+}
+
+1;
