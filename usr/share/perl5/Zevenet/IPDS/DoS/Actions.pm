1f8025d6a1720d3cc2bb1e3a45c31a96ff66f825
Jose Pablo <josepablo.garcia@zevenet.com>
2017-08-30 19:03:38 +0200

    [Bugfix] Load required module
    Signed-off-by: Jose Pablo <josepablo.garcia@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/IPDS/DoS/Actions.pm

--- usr/share/perl5/Zevenet/IPDS/DoS/Actions.pm
+++ usr/share/perl5/Zevenet/IPDS/DoS/Actions.pm
@@ -95,6 +95,8 @@ sub runDOSStartModule
 	# This block is a bugfix. When ssh_brute_force rule doesn't show the port
 	if ( ! &setDOSParam ( 'ssh_brute_force', 'port' ) )
 	{
+		require Zevenet::System::SSH;
+
 		my $sshconf = &getSsh();
 		my $port    = $sshconf->{ 'port' };
 		&setDOSParam ( 'ssh_brute_force', 'port', $port);
