bc4e76a53c2a8a8bcbca172d4d5533fb09261a00
Alvaro <alvaro.cano@zevenet.com>
2017-09-29 10:55:19 +0200

    [Bugfix] Fix modify SSH service
    Signed-off-by: Alvaro <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/System/SSH.pm

--- usr/share/perl5/Zevenet/System/SSH.pm
+++ usr/share/perl5/Zevenet/System/SSH.pm
@@ -165,7 +165,8 @@ sub setSsh
 
 	# restart service to apply changes
 	require Zevenet::IPDS::DoS::Config;
-	$output = system ( &getGlobalConfiguration( 'sshService' ) . " restart" );
+	my $cmd = &getGlobalConfiguration( 'sshService' ) . " restart";
+	$output = &logAndRun ( $cmd );
 	&setDOSParam( 'ssh_brute_force', 'port', $sshConf->{ 'port' } );
 
 	# restart sshbruteforce ipds rule if this is actived
