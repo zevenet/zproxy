fd7d286f19f9e6b54ad9175b0cefb4b90ddff92b
Alvaro <alvaro.cano@zevenet.com>
2017-10-24 09:32:12 +0200

    [Bugfix] Check ip rule table before than remove a entry
    Signed-off-by: Alvaro <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Net/Route.pm

--- usr/share/perl5/Zevenet/Net/Route.pm
+++ usr/share/perl5/Zevenet/Net/Route.pm
@@ -365,10 +365,13 @@ sub delRoutes    # ($table,$if_ref)
 			use Net::IPv4Addr qw(ipv4_network);
 			#~ Net::IPv4Addr->import;
 
-			my ( $net, $mask ) = ipv4_network( "$$if_ref{addr} / $$if_ref{mask}" );
-			$ip_cmd =
-			  "$ip_bin -$$if_ref{ip_v} rule del from $net/$mask table table_$$if_ref{name}";
-			$status = &logAndRun( "$ip_cmd" );
+			if ( &isRule( $if_ref ) )
+			{
+				my ( $net, $mask ) = ipv4_network( "$$if_ref{addr} / $$if_ref{mask}" );
+				$ip_cmd =
+					"$ip_bin -$$if_ref{ip_v} rule del from $net/$mask table table_$$if_ref{name}";
+				$status = &logAndRun( "$ip_cmd" );
+			}
 
 			return $status;
 		}
