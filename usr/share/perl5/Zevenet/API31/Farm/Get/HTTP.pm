f24f4598a072fb93590932578791eb70a073117c
Jose Pablo <josepablo.garcia@zevenet.com>
2017-12-20 18:07:48 +0100

    [Improvement] Make SNI lists optional
    Signed-off-by: Jose Pablo <josepablo.garcia@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/API31/Certificate/Farm.pm
    	modified:   usr/share/perl5/Zevenet/API31/Farm/Get/HTTP.pm

--- usr/share/perl5/Zevenet/API31/Farm/Get/HTTP.pm
+++ usr/share/perl5/Zevenet/API31/Farm/Get/HTTP.pm
@@ -47,22 +47,26 @@ sub get_farm_struct
 	elsif ( $httpverb == 3 ) { $httpverb = "MSextWebDAV"; }
 	elsif ( $httpverb == 4 ) { $httpverb = "MSRPCext"; }
 
-	my $type     = &getFarmType( $farmname );
+	my $type    = &getFarmType( $farmname );
 	my $certname;
-	my $cipher   = '';
-	my $ciphers  = 'all';
+	my $cipher  = '';
+	my $ciphers = 'all';
 	my @cnames;
 
 	if ( $type eq "https" )
 	{
-		require Zevenet::Farm::Ext;
 		require Zevenet::Farm::HTTP::HTTPS;
 
-		$certname = &getFarmCertificate( $farmname );
-		@cnames   = &getFarmCertificatesSNI( $farmname );
-		my $elem     = scalar @cnames;
+		if ( eval { require Zevenet::Farm::Ext; } )
+		{
+			@cnames = &getFarmCertificatesSNI( $farmname );
+		}
+		else
+		{
+			@cnames = ( &getFarmCertificate( $farmname ) );
+		}
 
-		for ( my $i = 0 ; $i < $elem ; $i++ )
+		for ( my $i = 0 ; $i < scalar @cnames ; $i++ )
 		{
 			push @out_cn, { file => $cnames[$i], id => $i + 1 };
 		}
@@ -71,7 +75,7 @@ sub get_farm_struct
 		$ciphers = &getFarmCipherSet( $farmname );
 		chomp ( $ciphers );
 
-		# adapt "ciphers" to required interface values 
+		# adapt "ciphers" to required interface values
 		if ( $ciphers eq "cipherglobal" )
 		{
 			$ciphers = "all";
@@ -142,13 +146,13 @@ sub farms_name_http # ( $farmname )
 	my @out_s;
 
 	# Services
-	my $services = &getHTTPFarmVS( $farmname, "", "" );
-	my @serv = split ( "\ ", $services );
+	my $services = &getHTTPFarmVS( $farmname, '', '' );
+	my @serv = split ( ' ', $services );
 
 	foreach my $s ( @serv )
 	{
 		my $serviceStruct = &getHTTPServiceStruct ( $farmname, $s );
-		
+
 		# Remove backend status 'undefined', it is for news api versions
 		foreach my $be (@{$serviceStruct->{ 'backends' }})
 		{
