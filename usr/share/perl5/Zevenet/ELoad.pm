67e92f7a7b282a1783782399736888a177a308bb
Alvaro Cano <alvaro.cano@zevenet.com>
2018-02-16 11:07:07 +0000

    [Bugfix] Add exception to update certificate file in enterprise.bin to API 3.2
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/local/zevenet/app/zbin/enterprise.bin
    	modified:   usr/share/perl5/Zevenet/ELoad.pm

--- usr/share/perl5/Zevenet/ELoad.pm
+++ usr/share/perl5/Zevenet/ELoad.pm
@@ -68,7 +68,7 @@ sub eload
 	if ( exists $ENV{ PATH_INFO } && $ENV{ PATH_INFO } eq '/certificates/activation' )
 	{
 		# escape '\n' characters in activation certificate
-		$req{ args }->[0] =~ s/\n/\\n/g,
+		$req{ args }->[0] =~ s/\n/\\n/g;
 	}
 
 	unless ( eval { $input = encode_json( $req{ args } ) } )
