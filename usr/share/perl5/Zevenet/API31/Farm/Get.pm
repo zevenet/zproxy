28144dcec04bf9b1b37d56d70f92a30983688e4a
Alvaro <alvaro.cano@zevenet.com>
2018-03-01 15:10:57 +0100

    [Bugfix] Redirect in /farm/<farmanme>/sumary to /farm/<farmname> when the farm is not http
    Signed-off-by: Alvaro <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/API31/Farm/Get.pm

--- usr/share/perl5/Zevenet/API31/Farm/Get.pm
+++ usr/share/perl5/Zevenet/API31/Farm/Get.pm
@@ -150,7 +150,10 @@ sub farms_name_summary    # ( $farmname )
 		require Zevenet::API31::Farm::Get::HTTP;
 		&farms_name_http_summary( $farmname );
 	}
-
+	else
+	{
+		&farms_name( $farmname );
+	}
 }
 
 #GET /farms/<name>
