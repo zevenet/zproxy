5064a4156dbdab78217c701ce6ab9209d5d2207e
Emilio Campos <emilio.campos@zevenet.com>
2018-03-26 17:01:39 +0200

    [bugfix] Field Issuer was not loaded with a correct value when certificates were listed
    Signed-off-by: Emilio Campos <emilio.campos@zevenet.com>
    
    	modified:   Certificate.pm

--- usr/share/perl5/Zevenet/Certificate.pm
+++ usr/share/perl5/Zevenet/Certificate.pm
@@ -202,7 +202,7 @@ sub getCertIssuer    # ($certfile)
 	}
 
 	$certissu = &getCleanBlanc( $certissu );
-
+	&zenlog("certissuer is $certissu");
 	return $certissu;
 }
 
@@ -619,7 +619,7 @@ sub getCertInfo    # ($certfile)
 	my $issuer = "";
 	if ( $type eq "Certificate" )
 	{
-		my ( $line ) = grep 'Issuer:', @cert_data;
+		my ( $line ) = grep (/Issuer:/, @cert_data);
 		( undef, $line ) = split ( /CN=/, $line );
 		( $issuer ) = split ( /\/emailAddress=/, $line );
 	}
