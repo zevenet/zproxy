a4be4a185c91ef7a35fc7d0d4d0404e2b94d264b
Jose Pablo <josepablo.garcia@zevenet.com>
2018-03-27 12:57:16 +0200

    [Improvement] Comment already defined activation certificate function in API 3
    Signed-off-by: Jose Pablo <josepablo.garcia@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Cmd/zapi-v3.pm

--- usr/share/perl5/Zevenet/Cmd/zapi-v3.pm
+++ usr/share/perl5/Zevenet/Cmd/zapi-v3.pm
@@ -105,164 +105,165 @@ include 'Zevenet::API3::Auth';
 ############################
 # Check certificate
 ############################
-sub get_sys_uuid
-{
-	my ( $dmi ) = grep ( /UUID\:/, `/usr/sbin/dmidecode` );
-	( undef, $dmi ) = split ( /:\s+/, $dmi );
 
-	chomp $dmi;
-
-	return $dmi;
-}
-
-# evaluate certificate
-sub certcontrol
-{
-	use Time::Local;
-	#~ use Zevenet::Config;
-	#~ use Zevenet::SystemInfo;
-
-	my $basedir = &getGlobalConfiguration( 'basedir' );
-	my $zlbcertfile = "$basedir/zlbcertfile.pem";
-	my $swcert = 0;
-
-	if ( ! -e $zlbcertfile )
-	{
-		#swcert = 1 ==> There isn't certificate
-		$swcert = 1;
-		return $swcert;
-	}
-
-	my $openssl_bin = "/usr/bin/openssl";
-	my $keyid       = "4B:1B:18:EE:21:4A:B6:F9:76:DE:C3:D8:86:6D:DE:98:DE:44:93:B9";
-	my @months      = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-	my $hostname    = &getHostname();
-	my $key         = &keycert();
-	my @zen_cert    = `$openssl_bin x509 -in $zlbcertfile -noout -text 2>/dev/null`;
-
-	if (    ( !grep /$key/, @zen_cert )
-		 || ( !grep /keyid:$keyid/, @zen_cert )
-		 || ( !grep /CN=$hostname\/|CN = $hostname\,/, @zen_cert ) )
-	{
-		#swcert = 2 ==> Cert isn't signed OK
-		$swcert = 2;
-		return $swcert;
-	}
-
-	# Certificate validity date
-	my ( $nb ) = grep /Not Before/i, @zen_cert;
-	$nb =~ s/.*not before.*:\ //i;
-
-	my ( $month, $day, $hours, $min, $sec, $year ) = split /[ :]+/, $nb;
-	( $month ) = grep { $months[$_] eq $month } 0..$#months;
-	my $ini = timegm( $sec, $min, $hours, $day, $month, $year );
-
-	# Certificate expiring date
-	my ( $na ) = grep /Not After/i, @zen_cert;
-	$na =~ s/.*not after.*:\ //i;
-
-	( $month, $day, $hours, $min, $sec, $year ) = split /[ :]+/, $na;
-	( $month ) = grep { $months[$_] eq $month } 0..$#months;
-	my $end = timegm( $sec, $min, $hours, $day, $month, $year );
-
-	# Validity remaining
-	my $totaldays = ( $end - $ini ) / 86400;
-	$totaldays =~ s/\-//g;
-	my $dayright = ( $end - time () ) / 86400;
-
-	if ( $dayright < 0 )
-	{
-		#control errors
-		if ( $totaldays < 364 )
-		{
-			# Policy: expired testing certificates would not stop zen service,
-			# but rebooting the service would not start the service,
-			# interfaces should always be available.
-			$swcert = 3;
-		}
-
-		if ( $totaldays > 364 )
-		{
-			# The contract support plan is expired you have to request a
-			# new contract support. Only message alert!
-			$swcert = -1;
-		}
-	}
-
-	# error codes
-	#swcert = 0 ==> OK
-	#swcert = 1 ==> There isn't certificate
-	#swcert = 2 ==> Cert isn't signed OK
-	#swcert = 3 ==> Cert test and it's expired
-	#swcert = -1 ==> Cert support and it's expired
-
-	#output
-	return $swcert;
-}
-
-
-# build local key
-sub keycert
-{
-	#~ use Zevenet::SystemInfo;
-
-	my $dmi      = get_sys_uuid();
-	my $hostname = &getHostname();
-
-	my $block1 = crypt ( "${dmi}${hostname}", "93" );
-	my $block2 = crypt ( "${hostname}${dmi}", "a3" );
-	my $block3 = crypt ( "${dmi}${hostname}", "ZH" );
-	my $block4 = crypt ( "${hostname}${dmi}", "h7" );
-	$block1 =~ s/^93//;
-	$block2 =~ s/^a3//;
-	$block3 =~ s/^ZH//;
-	$block4 =~ s/^h7//;
-
-	my $str = "${block1}-${block2}-${block3}-${block4}";
-
-	return $str;
-}
-
-sub checkActivationCertificate
-{
-	my $swcert = &certcontrol();
-
-	# if $swcert is greater than 0 zapi should not work
-	if ( $swcert > 0 )
-	{
-		my $msg;
-
-		if ( $swcert == 1 )
-		{
-			$msg =
-			  "There isn't a valid Zen Load Balancer certificate file, please request a new one";
-		}
-		elsif ( $swcert == 2 )
-		{
-			$msg =
-			  "The certificate file isn't signed by the Zevenet Certificate Authority, please request a new one";
-		}
-		elsif ( $swcert == 3 )
-		{
-			# Policy: expired testing certificates would not stop zen service,
-			# but rebooting the service would not start the service,
-			# interfaces should always be available.
-			$msg =
-			  "The Zen Load Balancer certificate file you are using is for testing purposes and its expired, please request a new one";
-		}
-
-		my $body = {
-					 message         => $msg,
-					 certificate_key => &keycert(),
-					 hostname        => &getHostname(),
-		};
-
-		return &httpResponse( { code => 403, body => $body } );
-	}
-
-	return $swcert;
-}
-
+#sub get_sys_uuid
+#{
+#	my ( $dmi ) = grep ( /UUID\:/, `/usr/sbin/dmidecode` );
+#	( undef, $dmi ) = split ( /:\s+/, $dmi );
+#
+#	chomp $dmi;
+#
+#	return $dmi;
+#}
+#
+## evaluate certificate
+#sub certcontrol
+#{
+#	use Time::Local;
+#	#~ use Zevenet::Config;
+#	#~ use Zevenet::SystemInfo;
+#
+#	my $basedir = &getGlobalConfiguration( 'basedir' );
+#	my $zlbcertfile = "$basedir/zlbcertfile.pem";
+#	my $swcert = 0;
+#
+#	if ( ! -e $zlbcertfile )
+#	{
+#		#swcert = 1 ==> There isn't certificate
+#		$swcert = 1;
+#		return $swcert;
+#	}
+#
+#	my $openssl_bin = "/usr/bin/openssl";
+#	my $keyid       = "4B:1B:18:EE:21:4A:B6:F9:76:DE:C3:D8:86:6D:DE:98:DE:44:93:B9";
+#	my @months      = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
+#	my $hostname    = &getHostname();
+#	my $key         = &keycert();
+#	my @zen_cert    = `$openssl_bin x509 -in $zlbcertfile -noout -text 2>/dev/null`;
+#
+#	if (    ( !grep /$key/, @zen_cert )
+#		 || ( !grep /keyid:$keyid/, @zen_cert )
+#		 || ( !grep /CN=$hostname\/|CN = $hostname\,/, @zen_cert ) )
+#	{
+#		#swcert = 2 ==> Cert isn't signed OK
+#		$swcert = 2;
+#		return $swcert;
+#	}
+#
+#	# Certificate validity date
+#	my ( $nb ) = grep /Not Before/i, @zen_cert;
+#	$nb =~ s/.*not before.*:\ //i;
+#
+#	my ( $month, $day, $hours, $min, $sec, $year ) = split /[ :]+/, $nb;
+#	( $month ) = grep { $months[$_] eq $month } 0..$#months;
+#	my $ini = timegm( $sec, $min, $hours, $day, $month, $year );
+#
+#	# Certificate expiring date
+#	my ( $na ) = grep /Not After/i, @zen_cert;
+#	$na =~ s/.*not after.*:\ //i;
+#
+#	( $month, $day, $hours, $min, $sec, $year ) = split /[ :]+/, $na;
+#	( $month ) = grep { $months[$_] eq $month } 0..$#months;
+#	my $end = timegm( $sec, $min, $hours, $day, $month, $year );
+#
+#	# Validity remaining
+#	my $totaldays = ( $end - $ini ) / 86400;
+#	$totaldays =~ s/\-//g;
+#	my $dayright = ( $end - time () ) / 86400;
+#
+#	if ( $dayright < 0 )
+#	{
+#		#control errors
+#		if ( $totaldays < 364 )
+#		{
+#			# Policy: expired testing certificates would not stop zen service,
+#			# but rebooting the service would not start the service,
+#			# interfaces should always be available.
+#			$swcert = 3;
+#		}
+#
+#		if ( $totaldays > 364 )
+#		{
+#			# The contract support plan is expired you have to request a
+#			# new contract support. Only message alert!
+#			$swcert = -1;
+#		}
+#	}
+#
+#	# error codes
+#	#swcert = 0 ==> OK
+#	#swcert = 1 ==> There isn't certificate
+#	#swcert = 2 ==> Cert isn't signed OK
+#	#swcert = 3 ==> Cert test and it's expired
+#	#swcert = -1 ==> Cert support and it's expired
+#
+#	#output
+#	return $swcert;
+#}
+#
+#
+## build local key
+#sub keycert
+#{
+#	#~ use Zevenet::SystemInfo;
+#
+#	my $dmi      = get_sys_uuid();
+#	my $hostname = &getHostname();
+#
+#	my $block1 = crypt ( "${dmi}${hostname}", "93" );
+#	my $block2 = crypt ( "${hostname}${dmi}", "a3" );
+#	my $block3 = crypt ( "${dmi}${hostname}", "ZH" );
+#	my $block4 = crypt ( "${hostname}${dmi}", "h7" );
+#	$block1 =~ s/^93//;
+#	$block2 =~ s/^a3//;
+#	$block3 =~ s/^ZH//;
+#	$block4 =~ s/^h7//;
+#
+#	my $str = "${block1}-${block2}-${block3}-${block4}";
+#
+#	return $str;
+#}
+#
+#sub checkActivationCertificate
+#{
+#	my $swcert = &certcontrol();
+#
+#	# if $swcert is greater than 0 zapi should not work
+#	if ( $swcert > 0 )
+#	{
+#		my $msg;
+#
+#		if ( $swcert == 1 )
+#		{
+#			$msg =
+#			  "There isn't a valid Zen Load Balancer certificate file, please request a new one";
+#		}
+#		elsif ( $swcert == 2 )
+#		{
+#			$msg =
+#			  "The certificate file isn't signed by the Zevenet Certificate Authority, please request a new one";
+#		}
+#		elsif ( $swcert == 3 )
+#		{
+#			# Policy: expired testing certificates would not stop zen service,
+#			# but rebooting the service would not start the service,
+#			# interfaces should always be available.
+#			$msg =
+#			  "The Zen Load Balancer certificate file you are using is for testing purposes and its expired, please request a new one";
+#		}
+#
+#		my $body = {
+#					 message         => $msg,
+#					 certificate_key => &keycert(),
+#					 hostname        => &getHostname(),
+#		};
+#
+#		return &httpResponse( { code => 403, body => $body } );
+#	}
+#
+#	return $swcert;
+#}
+#
 #########################################
 #
 # Debugging messages
