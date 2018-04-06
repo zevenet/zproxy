c30cbec98f647ad64b5dfb5df7b2d47d126893d2
Alvaro <alvaro.cano@zevenet.com>
2018-04-06 13:54:02 +0200

    [Bugfix] Parameter http "ignore 100 continue" is enabled for default
    Signed-off-by: Alvaro <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Farm/HTTP/Config.pm

--- usr/share/perl5/Zevenet/Farm/HTTP/Config.pm
+++ usr/share/perl5/Zevenet/Farm/HTTP/Config.pm
@@ -664,7 +664,7 @@ sub getHTTPFarm100Continue    # ($farm_name)
 	if ( $farm_type eq "http" || $farm_type eq "https" )
 	{
 		open FR, "<$configdir\/$farm_filename" or return $output;
-		$output = 0;	# if the sentence is not in config file, it is disabled
+		$output = 1;	# if the sentence is not in config file, it is enabled
 		my @file = <FR>;
 		foreach my $line ( @file )
 		{
