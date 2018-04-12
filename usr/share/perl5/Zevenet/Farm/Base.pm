398e74b4d37298e46e81e200776dfc5452a91a17
Alvaro Cano <alvaro.cano@zevenet.com>
2018-04-12 07:54:41 +0000

    [Bugfix] If a redirect exists and there are not backends, put farm status as UP
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/API31/Farm/Get/L4xNAT.pm
    	modified:   usr/share/perl5/Zevenet/Farm/Base.pm

--- usr/share/perl5/Zevenet/Farm/Base.pm
+++ usr/share/perl5/Zevenet/Farm/Base.pm
@@ -231,6 +231,23 @@ sub getFarmVipStatus    # ($farm_name)
 		$up_flag = 1 if $be->{ 'status' } eq "up";
 		$maintenance_flag = 1 if $be->{ 'status' } eq "maintenance";
 		$down_flag = 1 if $be->{ 'status' } eq "down";
+
+		# if there is a backend up and another down, the status is 'problem'
+		last if ( $down_flag and $up_flag );
+	}
+
+	# check if redirect exists when there are not backends
+	if ( $type =~ /http/ )
+	{
+		require Zevenet::Farm::HTTP::Service;
+		foreach my $srv ( &getHTTPFarmServices( $farm_name ) )
+		{
+			if ( &getHTTPFarmVS( $farm_name, $srv, 'redirect' ) )
+			{
+				$up_flag = 1;
+				last;
+			}
+		}
 	}
 
 	# Decision logic
