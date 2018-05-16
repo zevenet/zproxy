2a901ea9cf02f2286c3bff897f14f546ac992c58
Alvaro Cano <alvaro.cano@zevenet.com>
2018-05-16 10:08:21 +0000

    [Improvement] Increased Zevenet version
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   DEBIAN/control
    	modified:   DEBIAN/postinst
    	modified:   usr/local/zevenet/app/checkglobalconf/global.conf.tpl
    	new file:   usr/local/zevenet/app/zbin/add_srv_tag_pound.pl

--- usr/local/zevenet/app/checkglobalconf/global.conf.tpl
+++ usr/local/zevenet/app/checkglobalconf/global.conf.tpl
@@ -27,7 +27,7 @@ $rttables = "/etc/iproute2/rt_tables";
 #this file
 $globalcfg = "/usr/local/zevenet/config/global.conf";
 #version ZEVENET
-$version="5.1.9";#update
+$version="5.1.10";#update
 #appliance version file
 $applianceVersionFile="/etc/zevenet_version";
 #Cipher PCI
---
+++ usr/local/zevenet/app/zbin/add_srv_tag_pound.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+# This script is for a bugfix moving http services, where the tag '#ZWACL-END' was deleted
+
+use strict;
+use Tie::File;
+
+my $dir='/usr/local/zevenet/config';
+
+foreach my $i ( `ls $dir/*_pound.cfg` )
+{
+	chomp $i;
+	if ( system ( "egrep '\s*#ZWACL-END' $i >/dev/null 2>&1" ) )
+	{
+		&addline ( $i );
+	}
+}
+
+sub addline
+{
+	my $file_name = shift;
+
+	return if ( !-f $file_name );
+	print "Adding tag to $file_name\n";
+
+	tie my @file, 'Tie::File', $file_name;
+	my $flag;
+	for ( my $it=-1; $file[ $it ] !~ /^\s*User/ and !$flag ; $it-- ){
+		if ( $file[ $it ] =~ /^\s*End/ )
+		{
+			$flag = 1;
+			$file[ $it ] = "\t#ZWACL-END\nEnd";
+		}
+	}
+	untie @file;
+}
+
+exit ;
+
