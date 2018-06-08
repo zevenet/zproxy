b3c8afb023f15b718113c051ccfce1e6e937603e
Alvaro Cano <alvaro.cano@zevenet.com>
2018-06-08 11:17:44 +0000

    [Bugfix] Change call to script zcluster-manager
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Cluster.pm

--- usr/share/perl5/Zevenet/Cluster.pm
+++ usr/share/perl5/Zevenet/Cluster.pm
@@ -390,10 +390,10 @@ vrrp_instance ZCluster {
 \t\t$zcl_conf->{$remotehost}->{ip}
 \t}
 
-\tnotify_master \"/usr/local/zevenet/bin/zcluster-manager notify_master\"
-\tnotify_backup \"/usr/local/zevenet/bin/zcluster-manager notify_backup\"
-\tnotify_fault  \"/usr/local/zevenet/bin/zcluster-manager notify_fault\"
-\tnotify        \"/usr/local/zevenet/bin/zcluster-manager\"
+\tnotify_master \"/usr/local/zevenet/bin/zcluster-manager zcluster-manager notify_master\"
+\tnotify_backup \"/usr/local/zevenet/bin/zcluster-manager zcluster-manager notify_backup\"
+\tnotify_fault  \"/usr/local/zevenet/bin/zcluster-manager zcluster-manager notify_fault\"
+\tnotify        \"/usr/local/zevenet/bin/zcluster-manager zcluster-manager\"
 }
 
 ";
