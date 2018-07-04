b7c10840f2e34bfabb2f189944003d1c5e88c20a
Alvaro Cano <alvaro.cano@zevenet.com>
2018-07-04 06:48:47 +0000

    [Bugfix] Replace require of last commit
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Farm/GSLB/Factory.pm

--- usr/share/perl5/Zevenet/Farm/GSLB/Factory.pm
+++ usr/share/perl5/Zevenet/Farm/GSLB/Factory.pm
@@ -44,7 +44,7 @@ sub runGSLBFarmCreate    # ($vip,$vip_port,$farm_name)
 
 	require Zevenet::Farm::Core;
 	require Zevenet::Net::Util;
-	require Zevenet::GSLB::Config;
+	require Zevenet::Farm::GSLB::Config;
 
 	# get a control port not used
 	my $httpport;
