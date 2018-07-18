b4b5a70925a6d1cea6797d7d1cd44932c569baf0
Jose Pablo <josepablo.garcia@zevenet.com>
2018-07-18 12:07:20 +0200

    [Bugfix] Escape backslash to include it in string
    Signed-off-by: Jose Pablo <josepablo.garcia@zevenet.com>
    
    	modified:   usr/local/zevenet/app/zbin/add_srv_tag_pound.pl

--- usr/local/zevenet/app/zbin/add_srv_tag_pound.pl
+++ usr/local/zevenet/app/zbin/add_srv_tag_pound.pl
@@ -12,7 +12,7 @@ foreach my $i ( `ls $dir/*_pound.cfg 2>/dev/null` )
 {
 	chomp $i;
 
-	if ( system ( "egrep '\s*#ZWACL-END' $i >/dev/null 2>&1" ) )
+	if ( system ( "egrep '\\s*#ZWACL-END' $i >/dev/null 2>&1" ) )
 	{
 		&addline( $i );
 	}
