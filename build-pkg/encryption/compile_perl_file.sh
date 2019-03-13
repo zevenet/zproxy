0144f1359b973d3ff99bf5b80c0fe2d4f2e3d2fd
Alvaro Cano <alvaro.cano@zevenet.com>
2019-03-13 12:53:01 +0100

    [Bugfix] Remove encryption warning of enterprise.bin
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   build-pkg/encryption/compile_perl_file.sh

--- build-pkg/encryption/compile_perl_file.sh
+++ build-pkg/encryption/compile_perl_file.sh
@@ -11,14 +11,10 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 FILE="$1"
 NAME=$(basename "$FILE")
-FILECONTENTB64=$(openssl enc -aes-256-cbc -md md5 -nosalt -k "$SYMKEY" -in "$FILE" | base64 | tr -d '\n')
+FILECONTENTB64=$(openssl enc -aes-256-cbc -md md5 -nosalt -k "$SYMKEY" -in "$FILE" 2>/dev/null | base64 | tr -d '\n')
 
 echo -e "\nCompiling ${NAME} executable ..."
 sed "s|#{FILECONTENTB64}|${FILECONTENTB64}|g" "${BASE_DIR}/perlembed.c" > "${BASE_DIR}/${NAME}.c"
 sync
 gcc -o "${BASE_DIR}/${NAME}" "${BASE_DIR}/${NAME}.c" "${BASE_DIR}/perlxsi.c" $(perl -MExtUtils::Embed -e ccopts -e ldopts) -lcrypto
 strip "${BASE_DIR}/${NAME}"
-
-cp "${BASE_DIR}/${NAME}" "${FILE}.${PERLVERSION}"
-echo -n "Compiled file: "
-ls "${FILE}.${PERLVERSION}"
