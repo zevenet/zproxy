0aa1872cb1d7ea1fcb6e16902f043e16a87ac2a6
Alvaro Cano <alvaro.cano@zevenet.com>
2019-03-13 12:04:39 +0100

    [Bugfix] Remove encryption warning of enterprise.bin
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   build-pkg/encryption/encrypt_all.sh
    	modified:   usr/local/zevenet/bin/enterprise.bin

--- build-pkg/encryption/encrypt_all.sh
+++ build-pkg/encryption/encrypt_all.sh
@@ -2,12 +2,14 @@
 
 source encryption/config
 SYMKEY=1135628147310
+SALT="2D04E04389FBB423"
+ITER="14"
 
 echo -e "\nEncrypting modules"
 
 for file in "${encrypted_files[@]}"; do
 	printf "%-60s" "$file"
-	CMD="openssl aes-256-cbc -a -nosalt -k $SYMKEY -in $file -md md5"
+	CMD="openssl aes-256-cbc -a -salt -S $SALT -iter $ITER -k $SYMKEY -in $file -md md5"
 
 	if $CMD > "${file}e"; then
 		echo "[ OK ]"
