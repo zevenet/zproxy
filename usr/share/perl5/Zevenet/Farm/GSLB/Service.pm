d9a2b6eebbbbfbb6fc7948a1ddf2d8e6289fccac
Alvaro Cano <alvaro.cano@zevenet.com>
2019-03-22 12:43:29 +0000

    [Bugfix] It is remove the fist farm config file line when a service is deleted
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Farm/GSLB/FarmGuardian.pm
    	modified:   usr/share/perl5/Zevenet/Farm/GSLB/Service.pm

--- usr/share/perl5/Zevenet/Farm/GSLB/Service.pm
+++ usr/share/perl5/Zevenet/Farm/GSLB/Service.pm
@@ -766,7 +766,7 @@ sub existGSLBDefCheck
 	my $exist     = 0;
 
 	open my $fh, '<', $farm_file or return 0;
-	$exist = grep ( /tcp_$port =>)/, <$fh> );
+	$exist = grep ( /tcp_$port =>/, <$fh> );
 	close $fh;
 
 	return $exist;
@@ -777,7 +777,10 @@ sub addGSLBDefCheck
 	my ( $farm, $port ) = @_;
 
 	# do not add if it already exists
-	return 0 if ( &existGSLBDefCheck( $farm, $port ) );
+	if ( &existGSLBDefCheck( $farm, $port ) )
+	{
+		return 0;
+	}
 
 	require Zevenet::File;
 	my $newTcp =
