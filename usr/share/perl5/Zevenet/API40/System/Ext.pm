767cbdf6fd7f2be739309acb6e02100bab982e8b
Alvaro Cano <alvaro.cano@zevenet.com>
2019-04-16 09:01:02 +0200

    [New feature] Zapi call to list the availabled updates
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   build-pkg/config
    	modified:   usr/share/perl5/Zevenet/API40/Routes.pm
    	modified:   usr/share/perl5/Zevenet/API40/Routes/System.pm
    	new file:   usr/share/perl5/Zevenet/API40/System/Ext.pm
    	modified:   usr/share/perl5/Zevenet/API40/System/Info.pm
    	modified:   usr/share/perl5/Zevenet/Apt.pm

---
+++ usr/share/perl5/Zevenet/API40/System/Ext.pm
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+###############################################################################
+#
+#    Zevenet Software License
+#    This file is part of the Zevenet Load Balancer software package.
+#
+#    Copyright (C) 2014-today ZEVENET SL, Sevilla (Spain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+use strict;
+
+sub set_factory_reset
+{
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+
+	my $json_obj = shift;
+	my $desc     = "Apply a factory reset";
+
+	my $params = {
+				   "action" => {
+								 'values'    => ["apply"],
+								 'non_blank' => 'true',
+								 'required'  => 'true',
+				   },
+				   "interface" => {
+									'non_blank' => 'true',
+									'required'  => 'true',
+				   },
+				   "remove_backups" => {
+										 'values'    => ["true", "false"],
+										 'non_blank' => 'true',
+				   },
+				   "force" => {
+								'values'    => ["true", "false"],
+								'non_blank' => 'true',
+				   },
+	};
+
+	require Zevenet::Net::Interface;
+
+	# Check allowed parameters
+	my $error_msg = &checkZAPIParams( $json_obj, $params );
+	return &httpErrorResponse( code => 400, desc => $desc, msg => $error_msg )
+	  if ( $error_msg );
+
+	## The interface must be of NIC type and it has to be configured
+	my $if_ref = &getInterfaceConfig( $json_obj->{ interface } );
+	if ( $if_ref->{ type } ne 'nic' )
+	{
+		my $msg = "The interface has to be of type NIC.";
+		return &httpErrorResponse( code => 400, desc => $desc, msg => $msg );
+	}
+	elsif ( !$if_ref->{ addr } )
+	{
+		my $msg = "The interface has to be configured.";
+		return &httpErrorResponse( code => 400, desc => $desc, msg => $msg );
+	}
+
+	unless ( exists $json_obj->{ force } and $json_obj->{ force } eq 'true' )
+	{
+		my $msg =
+		  "While the execution of the factory reset process, the system will be restarted. "
+		  . "When the process will finish, the load balancer will be accesible by the ip $if_ref->{addr}. "
+		  . "If you are agree, execute again sending the parameter 'force'";
+		return &httpErrorResponse( code => 400, desc => $desc, msg => $msg );
+	}
+
+	my $remove_backups =
+	  ( $json_obj->{ remove_backups } eq 'true' ) ? 'remove-backups' : '';
+	if (
+		 &applyFactoryReset( $json_obj->{ interface }, $json_obj->{ remove_backups } ) )
+	{
+		my $msg = "Some error occurred applying the factory reset.";
+		return &httpErrorResponse( code => 400, desc => $desc, msg => $msg );
+	}
+
+	# The process will die before than here
+
+	my $msg =
+	  "The factroy reset was applied properly. The session will be lost. Please, try again in a while";
+	my $body = { description => $desc, message => $msg };
+	&httpResponse( { code => 200, body => $body } );
+}
+
+# GET /system/packages$
+sub get_packages_info
+{
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+	include 'Zevenet::Apt';
+
+	my $desc   = "Zevenet packages list info";
+	my $params = {};
+
+	my $output = &getAPTUpdatesList();
+
+	return &httpResponse(
+				 { code => 200, body => { description => $desc, params => $output } } );
+}
+
+1;
