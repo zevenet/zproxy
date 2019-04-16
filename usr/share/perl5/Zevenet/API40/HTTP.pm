36f3587a61ae25255fbade1699e1bf8c36ba06c0
Adan Samuel Marin Jacquot <adan.marin@zevenet.com>
2019-04-16 10:20:43 +0200

    [bugfix] Modified message type to info instead of error
    Signed-off-by: Adan Samuel Marin Jacquot <adan.marin@zevenet.com>
    
    	modificado:     ../usr/share/perl5/Zevenet/API40/HTTP.pm

--- usr/share/perl5/Zevenet/API40/HTTP.pm
+++ usr/share/perl5/Zevenet/API40/HTTP.pm
@@ -383,7 +383,7 @@ sub httpResponse    # ( \%hash ) hash_keys->( $code, %headers, $body )
 		# log error message on error.
 		if ( ref $self->{ body } eq 'HASH' )
 		{
-			&zenlog( "$self->{ body }->{ message }", "error", $LOG_TAG )
+			&zenlog( "$self->{ body }->{ message }", "info", $LOG_TAG )
 			  if ( exists $self->{ body }->{ message } );
 		}
 	}
