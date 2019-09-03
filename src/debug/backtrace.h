0deb0654676ea222a7486e0c5936f47541bd2534
Abdessamad El Abbassi <abdess.elabbassi@zevenet.com>
2019-09-03 16:17:47 +0200

    Fix backtrace symbol outputa and demangling using addr2line
    Signed-off-by: Abdessamad El Abbassi <abdess.elabbassi@zevenet.com>

--- src/debug/backtrace.h
+++ src/debug/backtrace.h
@@ -4,37 +4,84 @@
 
 #pragma once
 
-#include <functional>
-#include <string>
 #include <execinfo.h>
-#include "Debug.h"
-#include "../util/utils.h"
 #include <sys/resource.h>
 #include <cassert>
+#include <functional>
+#include <string>
+#include "../util/utils.h"
+#include "Debug.h"
+#include <execinfo.h>  // for backtrace
+#include <dlfcn.h>     // for dladdr
+#include <cxxabi.h>    // for __cxa_demangle
+#include <string>
+#include <sstream>
 
 namespace debug {
+
+static std::string addr2line(const std::string &bin,
+                             const std::string &address) {
+  char cmd[MAXBUF];
+  std::array<char, MAXBUF> buffer;
+  std::string result;
+  sprintf(cmd, "addr2line -s -a -p -f -C -e %s %s", bin.c_str(),
+          address.c_str());
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) {
+    return std::string();
+  }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  result.erase(std::remove(result.begin(), result.end(), '\n'), result.end());
+  auto pos = result.find_first_of(':');
+  if(pos != std::string::npos)
+      result = result.substr(pos + 1 , result.length());
+  return result;
+}
+
+static std::string addr2line(const std::string& symbol) {
+  std::string bin, addr;
+  auto s = symbol.find('(');
+  auto e = symbol.find(')');
+  if (std::string::npos != s) {
+    bin = symbol.substr(0, s);
+  }
+  if (std::string::npos != e) {
+    addr = std::string(symbol.data() + s  + 1, e - s - 1);
+  }
+//  Debug::logmsg(LOG_ERR, "[ bin: %s Addr: %s ]", bin.data(), addr.data());
+  return addr2line(bin, addr);
+}
+
 static void printBackTrace(int max_stack_size = 100) {
   void *frames[max_stack_size];
   int frame_count = ::backtrace(frames, max_stack_size);
   char **symbols = backtrace_symbols(frames, frame_count);
+  std::ostringstream trace_buf;
+  trace_buf << "Backtrace:";
   for (int frame_idx = 1; frame_idx < frame_count; ++frame_idx) {
+    trace_buf << "\n** " << frame_idx << "/" << frame_count - 1 << " ** ";
     if (symbols) {
-      Debug::logmsg(LOG_ERR, "** %d/%lu ** %s ", frame_idx, frame_count - 1, symbols[frame_idx]);
+      std::string debug_data = "";
+//#ifdef DEBUG
+      debug_data = addr2line(symbols[frame_idx]);
+//#endif
+      trace_buf << symbols[frame_idx] << " ** " <<  debug_data;
     } else {
-      Debug::logmsg(LOG_ERR, "** %d/%lu ** %s ", frame_idx, frame_count - 1, frames[frame_idx]);
+       trace_buf << frames[frame_idx];
     }
   }
+  Debug::logmsg(LOG_ERR, trace_buf.str().data());
 }
 
 // enable core dumps for debug builds on crash
-static void enableCoreDump(){
+static void enableCoreDump() {
   struct rlimit core_limit = {RLIM_INFINITY, RLIM_INFINITY};
   assert(setrlimit(RLIMIT_CORE, &core_limit) == 0);
 }
 
 static void EnableBacktraceOnTerminate() {
-  std::set_terminate([]() {
-    printBackTrace();
-  });
+  std::set_terminate([]() { printBackTrace(); });
 }
-}
+}  // namespace debug
