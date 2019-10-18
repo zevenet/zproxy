78eb057b639e7afbab6d0a1fd550dd101b2e90ed
Cristina Guerra <cristina.guerra@zevenet.com>
2019-10-18 11:32:26 +0200

    [Improvement] Migrating certificate directory with pound files
    Signed-off-by: Cristina Guerra <cristina.guerra@zevenet.com>

--- usr/local/zevenet/migrating/certificates.sh
+++ usr/local/zevenet/migrating/certificates.sh
@@ -4,7 +4,7 @@
 mv /usr/local/zevenet/config/{*.pem,*.csr,*.key} /usr/local/zevenet/config/certificates/ 2>/dev/null
 
 # Migrate certificate of farm config file
-for i in $(find /usr/local/zevenet/config/ -name "*_proxy.cfg");
+for i in $(find /usr/local/zevenet/config/ -name "*_proxy.cfg" -o -name "*_pound.cfg");
 do
 	if grep -q 'Cert \"\/usr\/local\/zevenet\/config\/\w*\.pem' $i; then
 		echo "Migrating certificate directory of config file"
