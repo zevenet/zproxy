05b68540fa9fdc6fbcbfacf83dc4045938a8c802
Alvaro Cano <alvaro.cano@zevenet.com>
2019-11-19 14:00:53 +0000

    [Improvement] Does not expect zapi key when zapi requests are sent from the load balancer
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Cmd/zapi-v4.0.pm

--- usr/share/perl5/Zevenet/Cmd/zapi-v4.0.pm
+++ usr/share/perl5/Zevenet/Cmd/zapi-v4.0.pm
@@ -88,12 +88,21 @@ require Zevenet::Zapi;
 # Session request
 require Zevenet::API40::Routes::Session if ( $q->path_info eq '/session' );
 
-# Verify authentication
-unless (    ( exists $ENV{ HTTP_ZAPI_KEY } && &validZapiKey() )
-		 or ( exists $ENV{ HTTP_COOKIE } && &validCGISession() ) )
+# allow requests from localhost
+if ( $ENV{ REMOTE_ADDR } eq '127.0.0.1' )
 {
-	&httpResponse(
-				   { code => 401, body => { message => 'Authorization required' } } );
+	require Zevenet::User;
+	&setUser( 'root' );
+}
+else
+{
+	# Verify authentication
+	unless (    ( exists $ENV{ HTTP_ZAPI_KEY } && &validZapiKey() )
+			 or ( exists $ENV{ HTTP_COOKIE } && &validCGISession() ) )
+	{
+		&httpResponse(
+					   { code => 401, body => { message => 'Authorization required' } } );
+	}
 }
 
 # log parameters passed
