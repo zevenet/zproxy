c62164a44a96a41c0c3fe854aac7764e1c00a69a
Alvaro Cano <alvaro.cano@zevenet.com>
2019-12-19 07:50:17 +0000

    [Bugfix] Add a check to validate zapi parameter that is missing
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Validate.pm

--- usr/share/perl5/Zevenet/Validate.pm
+++ usr/share/perl5/Zevenet/Validate.pm
@@ -193,7 +193,8 @@ my %format_re = (
 	'blacklists_minutes'   => $minutes,
 	'blacklists_period'    => $natural,
 	'blacklists_day'       => qr{(:?$dayofmonth|$weekdays)},
-	'blacklists_policy' => qr{(:?allow|deny)},    # not used from API 4
+	'blacklists_policy' => qr{(:?allow|deny)},      # not used from API 4
+	'blacklists_type'   => qr{(:?local|remote)},    # not used from API 4
 
 	# DoS
 	'dos_name'        => qr/[\w]+/,
