809a775b2be7558aab59556764a54794f5a5c5b7
Cristina Guerra <cristina.guerra@zevenet.com>
2020-01-21 17:48:36 +0100

    [Improvement] Call to system instead of logAndRun
    Signed-off-by: Cristina Guerra <cristina.guerra@zevenet.com>

--- usr/share/perl5/Zevenet/SystemInfo.pm
+++ usr/share/perl5/Zevenet/SystemInfo.pm
@@ -391,7 +391,7 @@ sub whereIam
 	{
 		$provider = "aws";
 	}
-	elsif ( !&logAndRun( "$dpkg -l | $grep waagent" ) )
+	elsif ( !system ( "$dpkg -l | $grep waagent &>/dev/null" ) )
 	{
 		$provider = "azure";
 	}
