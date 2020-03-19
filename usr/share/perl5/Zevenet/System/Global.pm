caafdcf264abe402c5bf6bd8e871bf4c4d0e57fb
Antonio Rendon <antonio.rendon@zevenet.com>
2020-03-19 11:27:48 +0000

    [Bugfix] Fix compiled calls
    Signed-off-by: Antonio Rendon <antonio.rendon@zevenet.com>
    
    	modified:   System/Global.pm

--- usr/share/perl5/Zevenet/System/Global.pm
+++ usr/share/perl5/Zevenet/System/Global.pm
@@ -94,7 +94,7 @@ sub setSystemGlobal
 		}
 		else
 		{
-			require 'Zevenet::Cluster';
+			include 'Zevenet::Cluster';
 			( $global->{ ssyncd } eq 'true' )
 			  ? &runZClusterRemoteManager( 'enable_ssyncd' )
 			  : &runZClusterRemoteManager( 'disable_ssyncd' );
@@ -157,7 +157,7 @@ sub setSystemGlobal
 
 		if ( ( !$err ) and ( $ssyncd_enabled eq "true" ) )
 		{
-			require Zevenet::Ssyncd;
+			include Zevenet::Ssyncd;
 			if ( &setSsyncdDisabled() )
 			{
 				&zenlog( "There was an error stopping Ssyncd", "debug2", "ssyncd" );
@@ -165,7 +165,7 @@ sub setSystemGlobal
 			}
 			else
 			{
-				require Zevenet::Cluster;
+				include Zevenet::Cluster;
 				&runZClusterRemoteManager( 'disable_ssyncd' );
 			}
 		}
