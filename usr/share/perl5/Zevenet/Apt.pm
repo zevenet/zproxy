9340f691c5756c079c4cffc98d5dfb0bc2652bfc
Alvaro Cano <alvaro.cano@zevenet.com>
2020-12-04 11:01:41 +0000

    [Bugfix] Reconfiguring APT if the certificate serial changed when checkupgrades is executed
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>
    
    	modified:   usr/share/perl5/Zevenet/Apt.pm

--- usr/share/perl5/Zevenet/Apt.pm
+++ usr/share/perl5/Zevenet/Apt.pm
@@ -6,6 +6,121 @@ use Zevenet::SystemInfo;
 include 'Zevenet::Certificate::Activation';
 
 my $cert_path = &getGlobalConfiguration( 'zlbcertfile_path' );
+my $openssl   = &getGlobalConfiguration( 'openssl' );
+my $grep      = &getGlobalConfiguration( 'grep_bin' );
+
+=begin nd
+Function: getAPTSerial
+
+    It returns the certificate serial used to sync with the APT
+
+Parameters:
+
+Returns:
+	string - serial
+
+=cut
+
+sub getAPTSerial
+{
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+
+	my $subserial = "$openssl x509 -in $cert_path -serial -noout";
+
+	# command to get the serial
+	my $serial = &logAndGet( $subserial );
+	if ( $serial eq '' )
+	{
+		&zenlog( "Serial is not correct", "error", "apt" );
+	}
+	else
+	{
+		# creating the structure that apt understands
+		$serial =~ s/serial=//;
+
+		# delete line break of the variable
+		$serial =~ s/[\r\n]//g;
+	}
+
+	return $serial;
+}
+
+=begin nd
+Function: getAPTSubjKeyId
+
+    It returns the certificate subject key ID
+
+Parameters:
+
+Returns:
+	string - subject key ID
+
+=cut
+
+sub getAPTSubjKeyId
+{
+	my $subkeyidentifier =
+	  "openssl x509 -in $cert_path -noout -text | $grep -A1 \"Subject Key Identifier\"";
+
+	# command to get the Subject Key Identifier
+	my $subjectkeyidentifier = `$subkeyidentifier`;
+	if ( $? != 0 )
+	{
+		&zenlog( "The subject ID '$subjectkeyidentifier' is not correct",
+				 "error", "apt" );
+		return "";
+	}
+
+	$subjectkeyidentifier =~ s/[\r\n]//g;
+	$subjectkeyidentifier =~ s/.*:\s+//g;
+
+	return $subjectkeyidentifier;
+}
+
+=begin nd
+Function: getAPTUserAgent
+
+    It returns the string used in the HTTP User Agent header to validate the load balancer
+    in the APT repository.
+
+Parameters:
+	cert serial -  This field is optional. The function will get it if it is not passed
+
+Returns:
+	string - value for the user agent header
+
+=cut
+
+sub getAPTUserAgent
+{
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+
+	my $serial = shift // &getAPTSerial();
+	my $subjectkeyidentifier = &getAPTSubjKeyId();
+
+	if ( $serial eq '' or $subjectkeyidentifier eq '' )
+	{
+		return '';
+	}
+
+	return "$serial:$subjectkeyidentifier";
+}
+
+=begin nd
+Function: setAPTRepo
+
+    It configures the system to connect with the APT.
+    It modify the apt.conf file (adding proxy info if it is configured), get the
+    gpg key, and the source list file.
+
+Parameters:
+
+Returns:
+	Integer - Error code, 0 on success or another value on failure
+
+=cut
 
 sub setAPTRepo
 {
@@ -13,37 +128,22 @@ sub setAPTRepo
 			 "debug", "PROFILING" );
 
 	# Variables
-	my $keyid     = &getKeySigned();
-	my $host      = &getGlobalConfiguration( 'repo_url_zevenet' );
-	my $openssl   = &getGlobalConfiguration( 'openssl' );
-	my $subserial = "$openssl x509 -in $cert_path -serial -noout";
-	my $subkeyid  = "$openssl x509 -in $cert_path -noout -text | grep \"$keyid\"";
-	my $subkeyidentifier =
-	  "openssl x509 -in $cert_path -noout -text | grep -A1 \"Subject Key Identifier\"";
-	my $file          = &getGlobalConfiguration( 'apt_source_zevenet' );
+	my $keyid    = &getKeySigned();
+	my $host     = &getGlobalConfiguration( 'repo_url_zevenet' );
+	my $subkeyid = "$openssl x509 -in $cert_path -noout -text | $grep \"$keyid\"";
+	my $file     = &getGlobalConfiguration( 'apt_source_zevenet' );
 	my $apt_conf_file = &getGlobalConfiguration( 'apt_conf_file' );
 	my $gpgkey        = &getGlobalConfiguration( 'gpg_key_zevenet' );
 	my $aptget_bin    = &getGlobalConfiguration( 'aptget_bin' );
 	my $aptkey_bin    = &getGlobalConfiguration( 'aptkey_bin' );
+	my $wget          = &getGlobalConfiguration( 'wget' );
 	my $distribution  = "buster";
 	my $kernel        = "4.19-amd64";
 
-	#get binaries
-	my $dpkg = &getGlobalConfiguration( 'dpkg_bin' );
-	my $grep = &getGlobalConfiguration( 'grep_bin' );
-	my $wget = &getGlobalConfiguration( 'wget' );
+	&zenlog( "Configuring the APT repository", "info", "SYSTEM" );
 
 	# Function call to configure proxy (Zevenet::SystemInfo)
 	&setEnv();
-
-	# check zevenet version. Versions prior to 5.2.5 will not be able to subscribe.
-	my $cmd = "$dpkg -l | $grep \"^ii\\s\\szevenet\\s*[0-9]\"";
-
-	my $version = &logAndGet( $cmd );
-
-	$version =~ s/[\r\n]//g;
-	$version =~ s/^ii\s\szevenet\s*//;
-	$version =~ s/\s*[a-zA-Z].*//;
 
 	# command to check keyid
 	# do not use the logAndRun function to obfuscate the signing cert keyid
@@ -54,30 +154,9 @@ sub setAPTRepo
 		return 1;
 	}
 
-	# command to get the serial
-	my $serial = &logAndGet( $subserial );
-	if ( $serial eq '' )
-	{
-		&zenlog( "Serial is not correct", "error", "apt" );
-		return 1;
-	}
-
-	# creating the structure that apt understands
-	$serial =~ s/serial=//;
-
-	# delete line break of the variable
-	$serial =~ s/[\r\n]//g;
-
-	# command to get the Subject Key Identifier
-	my $subjectkeyidentifier = `$subkeyidentifier`;
-	if ( $? != 0 )
-	{
-		&zenlog( "The subject ID '$subjectkeyidentifier' is not correct",
-				 "error", "apt" );
-		return 1;
-	}
-	$subjectkeyidentifier =~ s/[\r\n]//g;
-	$subjectkeyidentifier =~ s/.*:\s+//g;
+	my $serial    = &getAPTSerial();
+	my $userAgent = &getAPTUserAgent( $serial );
+	return 1 if ( $serial eq '' or $userAgent eq '' );
 
 	# adding key
 	my $error = &logAndRun(
@@ -93,11 +172,12 @@ sub setAPTRepo
 	# configuring user-agent
 	open ( my $fh, '>', $apt_conf_file )
 	  or die "Could not open file '$apt_conf_file' $!";
-	print $fh "Acquire { http::User-Agent \"$serial:$subjectkeyidentifier\"; };\n";
+	print $fh "Acquire { http::User-Agent \"$userAgent\"; };\n";
 	print $fh "Acquire::http::proxy \"\/\";\n";
 	print $fh "Acquire::https::proxy \"\/\";\n";
 	close $fh;
-	&setAPTProxy;
+
+	&setAPTProxy();
 
 	# get the kernel version
 	my $kernelversion = &getKernelVersion();
@@ -128,10 +208,31 @@ sub setAPTRepo
 	return 0;
 }
 
+=begin nd
+Function: getAPTUpdatesList
+
+    It returns information about the status of the system regarding updates.
+    This information is parsed from a file
+
+Parameters:
+
+Returns:
+	Hash ref -
+		{
+			 'message'    := message with the instructions to update the system
+			 'last_check' := date of the last time that checkupgrades (or apt-get) was executed
+			 'status'     := information about if there is pending updates.
+			 'number'     := number of packages pending of updating
+			 'packages'   := list of packages pending of updating
+		};
+
+=cut
+
 sub getAPTUpdatesList
 {
 	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
 			 "debug", "PROFILING" );
+
 	my $package_list = &getGlobalConfiguration( 'apt_outdated_list' );
 	my $message_file = &getGlobalConfiguration( 'apt_msg' );
 
@@ -179,41 +280,67 @@ sub getAPTUpdatesList
 	};
 }
 
-#check if local APT config is done
-#return 0 if OK
-#return 1 if APT is not configured
-sub getAPTConfig()
+=begin nd
+Function: getAPTConfig
+
+    It validates if the APT is properly configured.
+
+Parameters:
+
+Returns:
+	Integer - error code, 0 on success or another value on failure
+
+=cut
+
+sub getAPTConfig
 {
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
 
 	my $file          = &getGlobalConfiguration( 'apt_source_zevenet' );
 	my $apt_conf_file = &getGlobalConfiguration( 'apt_conf_file' );
 	use File::Grep;
 
-	if ( ( !-e $file ) or ( !-e $apt_conf_file ) )
+	if ( !-e $file or !-e $apt_conf_file )
 	{
 		&zenlog( "APT config files don't exist", "error", "apt" );
 		return 1;
-
 	}
+
+	my $userAgent = &getAPTUserAgent();
+
 	if (    ( !fgrep { /zevenet/ } $file )
-		 or ( !fgrep { /http::User-Agent/ } $apt_conf_file ) )
+		 or ( !fgrep { /http::User-Agent\s+\"$userAgent\"/ } $apt_conf_file ) )
 	{
 		&zenlog( "APT config is not done properly", "error", "apt" );
 		return 1;
-
 	}
-	&zenlog( "return 0" );
+
 	return 0;
 }
 
-#function used by checkupgrade in order to re-configre APT after any certificate key upload process
-sub setCheckUpgradeAPT()
+=begin nd
+Function: setCheckUpgradeAPT
+
+    This function is used by checkupgrade in order to re-configre APT after any
+    certificate key upload process
+
+Parameters:
+
+Returns:
+	none - .
+
+=cut
+
+sub setCheckUpgradeAPT
 {
-	if ( &getAPTConfig ne 0 )
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+
+	if ( &getAPTConfig != 0 )
 	{
 		&setAPTRepo();
 	}
-
 }
 
 =begin nd
@@ -261,7 +388,7 @@ sub uploadAPTIsoOffline
 		return 1;
 	}
 
-	if ( &logAndRun( "$file_bin $filepath | grep ISO" ) )
+	if ( &logAndRun( "$file_bin $filepath | $grep ISO" ) )
 	{
 		&zenlog( "The uploaded ISO doesn't look a valid ISO", 'error', 'apt' );
 		unlink $filepath;
@@ -290,6 +417,9 @@ Returns:
 
 sub setAPTProxy
 {
+	&zenlog( __FILE__ . ":" . __LINE__ . ":" . ( caller ( 0 ) )[3] . "( @_ )",
+			 "debug", "PROFILING" );
+
 	my $http_proxy    = &getGlobalConfiguration( 'http_proxy' );
 	my $https_proxy   = &getGlobalConfiguration( 'https_proxy' );
 	my $apt_conf_file = &getGlobalConfiguration( 'apt_conf_file' );
@@ -308,6 +438,5 @@ sub setAPTProxy
 		}
 	}
 	untie @apt_conf;
-
 }
 1;
