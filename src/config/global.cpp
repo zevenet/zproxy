86dfc4f47159cd4f9863bff93b80465499012ecc
nevola <laura.garcia@zevenet.com>
2021-02-10 18:51:22 +0100

    config: enable command line help options
    Enable the print proxy printed options:
    
    $ ./bin/zproxy -h
    zproxy[3077]:  zproxy starting...
    ./bin/zproxy, high-performance multithreaded and event-driven reverse proxy and load balancer
    Version 0.2.5-0 Copyright (C) 2021 ZEVENET
    Usage: ./bin/zproxy
      [ -h | --help ]			Show this help
      [ -s | --sync ]			Enable session synchronization
      [ -f <FILE> | --file <FILE> ]		Launch with the given configuration file
      [ -p <PIDFILE> | --pid <PIDFILE> ]	Set the PID file path
      [ -c | --check ]			Check the configuration without launching it
      [ -v | --verbose ]			Run in verbose mode
      [ -V | --version ]			Print the proxy version
    
    Signed-off-by: nevola <laura.garcia@zevenet.com>

--- src/config/global.cpp
+++ src/config/global.cpp
@@ -20,6 +20,7 @@
  */
 
 #include "global.h"
+
 global::run_options global::run_options::current{};
 global::StartOptions global::StartOptions::current{};
 
@@ -36,47 +37,81 @@ void global::StartOptions::setCurrent(const global::StartOptions& options) {
   current.sync_is_enabled = options.sync_is_enabled;
 }
 
+static void print_usage(const char *prog_name)
+{
+	fprintf(stderr,
+		"%s, high-performance multithreaded and event-driven reverse proxy and load balancer\n"
+		"Version %s %s\n"
+		"Usage: %s\n"
+		"  [ -h | --help ]			Show this help\n"
+		"  [ -s | --sync ]			Enable session synchronization\n"
+		"  [ -f <FILE> | --file <FILE> ]		Launch with the given configuration file\n"
+		"  [ -p <PIDFILE> | --pid <PIDFILE> ]	Set the PID file path\n"
+		"  [ -c | --check ]			Check the configuration without launching it\n"
+		"  [ -v | --verbose ]			Run in verbose mode\n"
+		"  [ -V | --version ]			Print the proxy version\n"
+		, prog_name, ZPROXY_VERSION, ZPROXY_COPYRIGHT, prog_name);
+}
+
+static const struct option options[] = {
+	{ .name = "help",		.has_arg = 0,	.val = 'h' },
+	{ .name = "sync",		.has_arg = 0,	.val = 's' },
+	{ .name = "file",		.has_arg = 1,	.val = 'f' },
+	{ .name = "pid",		.has_arg = 1,	.val = 'p' },
+	{ .name = "check",		.has_arg = 0,	.val = 'c' },
+	{ .name = "verbose",	.has_arg = 0,	.val = 'v' },
+	{ .name = "version",	.has_arg = 0,	.val = 'V' },
+	{ NULL },
+};
+
 std::unique_ptr<global::StartOptions> global::StartOptions::parsePoundOption(
-    int argc, char** argv, bool write_to_current) {
-  auto res = std::make_unique<StartOptions>();
-  int c_opt;
-  int opt_err = 0;
-  while ((c_opt = ::getopt(argc, argv, "sf:cvVp:")) > 0) switch (c_opt) {
-      case 's':
-        res->sync_is_enabled = true;
-        break;
-      case 'f':
-        res->conf_file_name = optarg;
-        break;
-      case 'p':
-        res->pid_file_name = optarg;
-        break;
-      case 'c':
-        res->check_only = true;
-        break;
-      case 'v':
-        res->verbose_mode = true;
-        break;
-      case 'V': {
-        Logger::logmsg(LOG_ALERT, "zproxy version %s", ZPROXY_VERSION);
-        Logger::logmsg(LOG_ALERT, "Build: %s %s", ZPROXY_HOST_INFO,
-                       ZPROXY_BUILD_INFO);
-        Logger::logmsg(LOG_ALERT, "%s", ZPROXY_COPYRIGHT);
-        std::exit(EXIT_FAILURE);
-      }
-      default: {
-        Logger::logmsg(LOG_ERR, "bad flag -%c", optopt);
-        return nullptr;
-      }
-    }
-  if (optind < argc) {
-    Logger::logmsg(LOG_ERR, "unknown extra arguments (%s...)", argv[optind]);
-    exit(EXIT_FAILURE);
-  }
-  // we must write to the current configuration the first time we parse the
-  // options
-  if (write_to_current) {
-    current.setCurrent(*res);
-  }
-  return std::move(res);
+	int argc, char** argv, bool write_to_current)
+{
+	auto res = std::make_unique<StartOptions>();
+	int c;
+	int opt_err = 0;
+
+	while ((c = getopt_long(argc, argv, "hsf:cvVp:", options, NULL)) != -1) {
+		switch (c) {
+		case 'h':
+			print_usage(argv[0]);
+			exit(EXIT_SUCCESS);
+		case 's':
+			res->sync_is_enabled = true;
+			break;
+		case 'f':
+			res->conf_file_name = optarg;
+			break;
+		case 'p':
+			res->pid_file_name = optarg;
+			break;
+		case 'c':
+			res->check_only = true;
+			break;
+		case 'v':
+			res->verbose_mode = true;
+			break;
+		case 'V':
+			Logger::logmsg(LOG_ALERT, "zproxy version %s", ZPROXY_VERSION);
+			Logger::logmsg(LOG_ALERT, "Build: %s %s", ZPROXY_HOST_INFO,
+						   ZPROXY_BUILD_INFO);
+			Logger::logmsg(LOG_ALERT, "%s", ZPROXY_COPYRIGHT);
+			exit(EXIT_FAILURE);
+		default:
+			Logger::logmsg(LOG_ERR, "bad flag -%c", optopt);
+			return nullptr;
+		}
+	}
+
+	if (optind < argc) {
+		Logger::logmsg(LOG_ERR, "unknown extra arguments (%s...)", argv[optind]);
+		exit(EXIT_FAILURE);
+	}
+
+	// we must write to the current configuration the first time we parse the
+	// options
+	if (write_to_current)
+		current.setCurrent(*res);
+
+	return std::move(res);
 }
