767fadc55158352668f561687c10dc0ab10ff9c8
Alvaro Cano <alvaro.cano@zevenet.com>
2021-03-19 10:39:23 +0100

    indent: update the indent script to apply the Linux Kernel code style
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- indent.sh
+++ indent.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-indent -i8 -br -npcs -npsl -nbc $1
+directories="zcutils src"
+
+for dir in $directories
+do
+	for file in `find $dir -regextype egrep -regex '.*\.(h|c|cpp)$'`;
+	do
+		echo  "replacing: $file"
+		indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 \
+-cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai \
+-saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 $file
+	done
+done
+
+# replace files
 find . -name "*~" -exec rm -rf {} \;
