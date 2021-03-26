e56789821bd53034404617b6c404170b7cfdbf5b
Alvaro Cano <alvaro.cano@zevenet.com>
2021-03-26 14:15:02 +0100

    indent: if the indent script receives an argument, it only will apply the format in that file
    Signed-off-by: Alvaro Cano <alvaro.cano@zevenet.com>

--- indent.sh
+++ indent.sh
@@ -2,16 +2,25 @@
 
 directories="zcutils src"
 
-for dir in $directories
-do
-	for file in `find $dir -regextype egrep -regex '.*\.(h|c|cpp)$'`;
-	do
-		echo  "replacing: $file"
+
+function apply () {
+	echo  "replacing: $1"
 		indent -nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 \
 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai \
--saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 $file
+-saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 $1
+}
+
+if [[ $1 ]]; then
+	apply $1
+else
+	for dir in $directories
+	do
+		for file in `find $dir -regextype egrep -regex '.*\.(h|c|cpp)$'`;
+		do
+			apply $file
+		done
 	done
-done
+fi
 
 # replace files
 find . -name "*~" -exec rm -rf {} \;
