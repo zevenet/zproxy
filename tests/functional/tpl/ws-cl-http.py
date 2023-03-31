73a1ead246be783276af281dfb3afa9b366fe05a
Nicolás A. Ortega Froysa <nicolas.ortega@zevenet.com>
2023-03-31 14:52:36 +0200

    tests: Thread websocket server & client
    This should allow us to make more flexible tests regarding websocket, both on
    the server and client side.
    
    Signed-off-by: Nicolás A. Ortega Froysa <nicolas.ortega@zevenet.com>

--- tests/functional/tpl/ws-cl-http.py
+++ tests/functional/tpl/ws-cl-http.py
@@ -16,15 +16,20 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Usage: ws-cl-http.py <host> <port> <ssl> <path>
+
 import socket
 import time
 import sys
 import ssl
+from threading import Thread
 
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 SSL = int(sys.argv[3])
 PATH = sys.argv[4]
+
+running = True
 
 ws_req = (
         bytes('GET {} HTTP/1.1\r\n'.format(PATH), 'utf-8') +
@@ -36,37 +41,60 @@ ws_req = (
         )
 
 if SSL > 0:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        context = ssl._create_unverified_context()
-        sock = context.wrap_socket(s)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    context = ssl._create_unverified_context()
+    sock = context.wrap_socket(s)
 else:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def handle_recv():
+    global running
+    global sock
+
+    while running:
+        try:
+            resp_msg = sock.recv(128)
+        except:
+            break
+
+        if not resp_msg:
+            break
+
+        print("WS Received: {!r}".format(resp_msg))
 
 serv_addr = (HOST, PORT)
 print("Connecting to {}:{}".format(*serv_addr))
 sock.connect(serv_addr)
 
+recv_thread = Thread(target=handle_recv)
 try:
+    # Upgrade protocols
     sock.sendall(ws_req)
 
-    is_done = False
     resp_msg = b""
-
-    while not is_done:
+    while True:
         data = sock.recv(128)
         resp_msg += data
-        if b'\r\n\r\n' in resp_msg:
-            is_done = True
+        if b"\r\n\r\n" in resp_msg:
+            break
 
     print("Received: {!r}".format(resp_msg))
+    if not b"101 Switching Protocols" in resp_msg:
+        print("ERROR: Didn't upgrade protocols")
+        raise Exception("Server didn't upgrade protocols")
+
+    # in WebSocket mode
+    recv_thread.start()
 
     for _ in range(10):
         sock.sendall(b"Hello from the client!")
-        data = sock.recv(128)
-        print("WS Received: {!r}".format(data))
-        time.sleep(1)
+        time.sleep(0.1)
 
     sock.sendall(b"close")
 
+    running = False
+    time.sleep(0.5)
 finally:
+    sock.shutdown(socket.SHUT_RDWR)
+    recv_thread.join()
     sock.close()
--- tests/functional/tpl/ws-sv-http.py
+++ tests/functional/tpl/ws-sv-http.py
@@ -20,14 +20,11 @@ import signal
 import socket
 import sys
 import ssl
+from threading import Thread
 
 SSL = int(sys.argv[1])
 
-def handle_exit(signal, frame):
-    print("Exit")
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, handle_exit)
+running = True
 
 ws_resp = (
         b'HTTP/1.1 101 Switching Protocols\r\n'
@@ -36,7 +33,43 @@ ws_resp = (
         b'Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=\r\n\r\n'
         )
 
+def handle_client(conn):
+    buf = b""
+    ws_mode = False
+    global running
+    while running:
+        try:
+            req_data = conn.recv(128)
+        except:
+            break
+
+        if not req_data:
+            break
+        elif not ws_mode:
+            buf += req_data
+            if b"\r\n\r\n" in buf:
+                print("Received: {!r}".format(buf))
+                if b"Upgrade: websocket" in buf:
+                    ws_mode = True
+                    global ws_resp
+                    conn.sendall(ws_resp)
+                buf = b""
+        else:
+            print("WS Received: {!r}".format(req_data))
+            conn.sendall(b"Hello from the server!")
+
+    conn.close()
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def handle_exit(signal, frame):
+    print("Exit")
+    global running
+    running = False
+    global sock
+    sock.shutdown(socket.SHUT_RDWR)
+
+signal.signal(signal.SIGINT, handle_exit)
 
 if SSL > 0:
     serv_addr = ('', 8443)
@@ -45,42 +78,28 @@ else:
 print("Running on {}:{}".format(*serv_addr))
 sock.bind(serv_addr)
 
-sock.listen(1)
+sock.listen()
 
-while True:
-    if SSL > 0:
-        raw_conn, cl_addr = sock.accept()
-        try:
-                conn = ssl.wrap_socket(raw_conn, server_side=True, certfile="nginx.pem",
-                        keyfile="nginx.key")
-        except:
-                # if exception is caused, this connection is from the zproxy
-                # monitor
-                continue
-    else:
-        conn, cl_addr = sock.accept()
+cl_threads = []
 
+while running:
     try:
-        req_data = b""
-        while True:
-            data = conn.recv(128)
-            if data == b'':
-                break
+        if SSL > 0:
+            raw_conn, cl_addr = sock.accept()
+            conn = ssl.wrap_socket(raw_conn, server_side=True, certfile="nginx.pem",
+                    keyfile="nginx.key")
+        else:
+            conn, cl_addr = sock.accept()
+    except:
+        # if exception is caused, this connection is from the zproxy
+        # monitor
+        continue
 
-            req_data += data
-            if b'\r\n\r\n' in req_data:
-                break
+    cl_thr = Thread(target=handle_client, args=[conn,])
+    cl_thr.start()
+    cl_threads.append(cl_thr)
 
-        if not req_data:
-            continue
+    cl_threads = [ t for t in cl_threads if t.is_alive() ]
 
-        print("Received: {!r}".format(req_data))
-        conn.sendall(ws_resp)
-
-        while data != b"close":
-            data = conn.recv(128)
-            print("WS Received: {!r}".format(data))
-            conn.sendall(b"Hello from the server!")
-
-    finally:
-        conn.close()
+for t in cl_threads:
+    t.join()
